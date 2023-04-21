dfd9b823e4d66cfa0b3decf99ba98f406d08a217
trantrongw <trantrongnghia1991@gmail.com>
2023-04-21 15:58:30 +0700

    Update streamlit_app.py
    add more code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('my parrents new healthy dinner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
